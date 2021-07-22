bugs fix of exemplars mat calculation

dfc_clustering.py
@@ -16,14 +16,21 @@ if __name__ == '__main__':
     number_of_subjects = 50
     in_dir = Path('/media/neuro/LivnyLab/Research/TBI_magneton/Analyses/MatriLan/matrices/Mat_180521/FC/Dynamic_Correlations/r_val/no_TH')
     [features_mat, num_of_windows] = create_features_matrix(in_dir, window_folder, TH, number_of_subjects)
-    scaler = StandardScaler()
-    all_subs_features_map = []
+    all_subs_exemplars_mat = []
     for sub_num in range(number_of_subjects):
-        one_subject_features_mat = features_mat[int(sub_num*num_of_windows):int(sub_num*num_of_windows+num_of_windows)]
-        one_subject_exemplar_mat = calc_subject_exemplars(one_subject_features_mat, peaks_threshold=0.3, plot=False)
-        all_subs_exemplars_mat = np.c_[all_subs_features_map, one_subject_exemplar_mat]
-# here I need to take all the matrices coming back from calc_subject_exemplars and append them to one matrix-->all_subs_features_map
+        one_subject_features_mat = features_mat[int(sub_num*num_of_windows):int((sub_num+1)*num_of_windows)]
+        one_subject_exemplar_mat = calc_subject_exemplars(one_subject_features_mat, peaks_threshold=0.2, plot=False)
+        all_subs_exemplars_mat.append(one_subject_exemplar_mat)
+        if len(one_subject_exemplar_mat) > 0:
+            print(f"\nsubject num {sub_num} has {len(one_subject_exemplar_mat)} exemplars")
+        else:
+            print(f"\nsubject num {sub_num} is empty!")
 
+    # here I need to take all the matrices coming back from calc_subject_exemplars and append them to one matrix-->all_subs_features_map
+
+    all_subs_exemplars_mat = np.concatenate(all_subs_exemplars_mat, axis=0)
+    print(f'\n{all_subs_exemplars_mat.shape}\n')
+    scaler = StandardScaler()
     scaled_features = scaler.fit_transform(all_subs_exemplars_mat)    # fisher transformation
     sse = []
     K_range = range(1, 11)

utils.py
@@ -8,7 +8,7 @@ from scipy.stats import zscore
 
 def create_features_matrix(in_dir: Path, window_folder: str, threshold=0.2, number_of_subjects=50):
     list_of_vecs = []
-    csv_files_list = list(in_dir.joinpath(window_folder).rglob('*.csv'))
+    csv_files_list = sorted(list(in_dir.joinpath(window_folder).rglob('*.csv')))
     for p in tqdm(csv_files_list):
         correlation_mat = np.loadtxt(p, delimiter=',')
         n_rows = correlation_mat.shape[0]